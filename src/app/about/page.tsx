99631a4bd1096c5506984f7f5d4f5d160fa03b8a
mu2hammad <muhammad.hammad@sdo.shj.ae>
2024-03-25 10:13:12 +0400

    contact us

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,15 @@
 import Info from "../components/info";
 
 export default function AboutPage() {
-    return <div>
-    <h1 className="text-xl md:text-3xl font-ibm-plex-sans-500 md:font-ibm-plex-sans-400 mb-1">Contact us</h1>
-    <p className="font-ibm-plex-sans-400 text-sm md:text-xl mb-8">Contact us for your inquiries and we will respond back within hours.</p>
-    <Info/>
+  return (
+    <div className="px-[var(--layout-padding-md)] md:px-0">
+      <h1 className="text-xl md:text-3xl font-ibm-plex-sans-500 md:font-ibm-plex-sans-400 mb-1">
+        Contact us
+      </h1>
+      <p className="font-ibm-plex-sans-400 text-sm md:text-xl mb-8">
+        Contact us for your inquiries and we will respond back within hours.
+      </p>
+      <Info />
     </div>
+  );
 }
