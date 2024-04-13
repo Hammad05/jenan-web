301591ca6e0ac2dd67ae158b48af83d4f89352fa
mu2hammad <muhammad.hammad@sdo.shj.ae>
2024-04-13 14:06:07 +0400

    logo fi

--- src/app/servicedetail/newbusiness/page.tsx
+++ src/app/servicedetail/newbusiness/page.tsx
@@ -31,7 +31,7 @@ export default function newBusinessSetup() {
           with all government authorities
         </p>
         <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 mt-5 divide-y divide-x divide-gray border-b border-r border-gray">
-          <div className="flex items-center border-l border-t border-gray px-6">
+          <div className="flex items-center border-l border-t border-gray px-2.5">
             <Image
               src="/Logo12dubaiEconomy.png"
               alt="Logo"
@@ -39,7 +39,7 @@ export default function newBusinessSetup() {
               height={154}
             />
           </div>
-          <div className="flex items-center px-10 py-8">
+          <div className="flex items-center px-6">
             <Image
               src="/Logo04dbxeconomyandtourism.png"
               alt="Logo"
@@ -47,10 +47,10 @@ export default function newBusinessSetup() {
               height={154}
             />
           </div>
-          <div className="flex items-center px-6 py-8">
+          <div className="flex items-center px-5">
             <Image src="/Logo06dmcc.png" alt="Logo" width={200} height={154} />
           </div>
-          <div className="flex items-center px-6">
+          <div className="flex items-center px-3.5">
             <Image
               src="/Logo11dbxintfincenter.png"
               alt="Logo"
@@ -58,13 +58,13 @@ export default function newBusinessSetup() {
               height={154}
             />
           </div>
-          <div className="flex items-center justify-center">
+          <div className="flex items-center justify-center px-12">
             <Image src="/Logo05shams.png" alt="Logo" width={200} height={200} />
           </div>
-          <div className="flex items-center px-12 py-6">
+          <div className="flex items-center px-6">
             <Image src="/Logo09jazfa.png" alt="Logo" width={200} height={154} />
           </div>
-          <div className="flex items-center justify-center">
+          <div className="flex items-center justify-center px-4">
             <Image
               src="/Logo07ajmanfreezone.png"
               alt="Logo"
@@ -72,7 +72,7 @@ export default function newBusinessSetup() {
               height={154}
             />
           </div>
-          <div className="flex items-center px-12 py-8">
+          <div className="flex items-center px-12">
             <Image
               src="/Logo10dbxairportfreezone.png"
               alt="Logo"
@@ -80,10 +80,10 @@ export default function newBusinessSetup() {
               height={154}
             />
           </div>
-          <div className="flex items-center px-16 py-8">
+          <div className="flex items-center px-12">
             <Image src="/Logo08rakez.png" alt="Logo" width={200} height={154} />
           </div>
-          <div className="flex items-center p-8">
+          <div className="flex items-center px-8">
             <Image
               src="/Logo03creativecity.png"
               alt="Logo"
@@ -91,7 +91,7 @@ export default function newBusinessSetup() {
               height={154}
             />
           </div>
-          <div className="flex items-center px-12 py-4">
+          <div className="flex items-center px-12">
             <Image
               src="/Logo02shjEcoDept.png"
               alt="Logo"
@@ -99,7 +99,7 @@ export default function newBusinessSetup() {
               height={154}
             />
           </div>
-          <div className="flex items-center px-8 py-4">
+          <div className="flex items-center px-8">
             <Image
               src="/Logo01ajmandeptofeconomics.png"
               alt="Logo"
