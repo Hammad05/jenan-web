06cdffab43d9020279f1e80e20e15d3033562acc
mu2hammad <muhammad.hammad@sdo.shj.ae>
2024-04-13 13:45:58 +0400

    contact info content adjustment

--- src/app/components/contact.tsx
+++ src/app/components/contact.tsx
@@ -14,19 +14,27 @@ export default function ContactInfo() {
     <div className="h-[64px] flex items-center">
       <button
         onClick={handleWhatsappClick}
-        className="bg-green flex-1 md:flex-auto px-4 h-full flex items-center justify-center gap-3 md:gap-2"
+        className="bg-green flex-1 md:flex-auto px-4 h-full flex items-center justify-center gap-3 md:gap-4"
       >
-        <img src="/whatsapp.svg" alt="Whatsapp" className="object-contain w-6 h-6 md:w-auto md:h-auto" />
-        <span className="text-sm md:text-base text-white">
+        <img
+          src="/whatsapp.svg"
+          alt="Whatsapp"
+          className="object-contain w-6 h-6 md:w-auto md:h-auto"
+        />
+        <span className="text-sm md:text-base text-white flex-initial">
           Chat on Whatsapp
         </span>
       </button>
       <button
         onClick={handleCallClick}
-        className="bg-black flex-1 md:flex-auto px-4 h-full flex items-center justify-center gap-3 md:gap-2"
+        className="bg-black flex-1 md:flex-auto px-4 h-full flex items-center justify-center gap-3 md:gap-4"
       >
-        <img src="/call.svg" alt="Call" className="object-contain w-6 h-6 md:w-auto md:h-auto" />
-        <span className="text-sm md:text-base text-white">
+        <img
+          src="/call.svg"
+          alt="Call"
+          className="object-contain w-6 h-6 md:w-auto md:h-auto"
+        />
+        <span className="text-sm md:text-base text-white flex-initial">
           +971 55 796 7733
         </span>
       </button>
